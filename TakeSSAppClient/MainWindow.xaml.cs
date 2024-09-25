bb1aa2585e7053e83e5c73841d0c6ff89685d828
Elvin0802 <elvincode1517@gmail.com>
2024-09-25 18:55:42 +0400

    Project adding. v: 1.0

---
+++ TakeSSAppClient/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Xaml;
+
+namespace TakeSSAppClient;
+
+public partial class MainWindow : Window
+{
+	public Socket ClientSocket { get; set; }
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		ServerPath = @$"..\..\..\..\TakeScreenShotAppServer\bin\Debug\net8.0\TakeScreenShotAppServer.exe";
+
+		ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		ClientSocket.Bind(new IPEndPoint(IPAddress.Loopback, 27001));
+
+		B = null;
+	}
+
+	public Process? Server { get; set; }
+	public string ServerPath { get; init; }
+	public Bitmap B { get; set; }
+
+	private void StartBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Server = Process.Start(ServerPath);
+
+		EndPoint ServerEndPoint = new IPEndPoint(IPAddress.Any, 0);
+		var totalBuffer = new byte[4];
+
+		ClientSocket.ReceiveFrom(totalBuffer, ref ServerEndPoint);
+
+		int total = BitConverter.ToInt32(totalBuffer, 0);
+
+		List<byte[]> byteList = new List<byte[]>();
+
+		for (int Num = 0; Num < total; Num++)
+		{
+			bool isRecieved = false;
+
+			while (!isRecieved)
+			{
+				var arr = new byte[32772];
+				int length = ClientSocket.ReceiveFrom(arr, ref ServerEndPoint);
+
+				int receivedNum = BitConverter.ToInt32(arr, 0);
+
+				byte[] data = new byte[length - 4];
+
+				Buffer.BlockCopy(arr, 4, data, 0, length - 4);
+
+				if (receivedNum == Num)
+				{
+					byteList.Add(data);
+
+					isRecieved = true;
+
+					ClientSocket.SendTo(BitConverter.GetBytes(receivedNum), ServerEndPoint);
+				}
+			}
+		}
+
+		using (MemoryStream ms = new MemoryStream())
+		{
+			foreach (var i in byteList)
+				ms.Write(i, 0, i.Length);
+
+			B = new Bitmap(ms);
+			ImageDisplayer.Source = BitmapToBitmapImage(B);
+		}
+	}
+
+	public BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+	{
+		using (MemoryStream ms = new MemoryStream())
+		{
+			bitmap.Save(ms, ImageFormat.Png);
+			ms.Seek(0, SeekOrigin.Begin);
+
+			BitmapImage b = new();
+			b.BeginInit();
+			b.StreamSource = ms;
+			b.CacheOption = BitmapCacheOption.OnLoad;
+			b.EndInit();
+			b.Freeze();
+
+			return b;
+		}
+	}
+
+	private void SaveBtn_Click(object sender, RoutedEventArgs e)
+	{
+		if (B is null) return;
+
+		string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+		path += "\\UDP_Screenshots";
+		
+		if (!File.Exists(path))
+			Directory.CreateDirectory(path);
+
+		var d = DateTime.Now;
+		var name = $"\\pic_{d.Year}{d.Month}{d.Day}_{d.Hour}{d.Minute}{d.Second}_{d.Millisecond}_ss.png";
+
+		var s = File.Create(path + name);
+
+		MemoryStream ms = new MemoryStream();
+
+		B.Save(ms, ImageFormat.Png);
+
+		s.Write(ms.ToArray());
+
+		MessageBox.Show("Screenshot Saved to Desktop.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
---
+++ TakeScreenShotAppServer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WindowHeight = 49;
+		Console.WindowWidth = 99;
+
+		Console.WriteLine("Taking screenshot.");
+		
+		TakeScreenShot();
+
+		Console.WriteLine("Screenshot taken and sent.");
+	}
+
+	public static void TakeScreenShot()
+	{
+		try
+		{
+			Rectangle bounds = new Rectangle(0, 0, 1920, 1080);
+			Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+			using (Graphics g = Graphics.FromImage(bitmap))
+			{
+				g.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+			}
+
+			var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			var clientEndPoint = new IPEndPoint(IPAddress.Loopback, 27001);
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				bitmap.Save(ms, ImageFormat.Png);
+				byte[] imageBytes = ms.ToArray();
+
+				int size = 32768;  // 32 KB
+				int total = (imageBytes.Length + size - 1) / size;
+
+				serverSocket.SendTo(BitConverter.GetBytes(total), clientEndPoint);
+
+				for (int num = 0; num < total; num++)
+				{
+					int currentSize = Math.Min(size, imageBytes.Length - (num * size));
+					byte[] data = new byte[currentSize];
+
+					Buffer.BlockCopy(imageBytes, num * size, data, 0, currentSize);
+
+					bool isContinue = false;
+
+					while (!isContinue)
+					{
+						byte[] message = BitConverter.GetBytes(num).Concat(data).ToArray();
+						serverSocket.SendTo(message, clientEndPoint);
+
+						var arr = new byte[4];
+						EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+						serverSocket.ReceiveFrom(arr, ref endPoint);
+						int currentNum = BitConverter.ToInt32(arr, 0);
+
+						if (currentNum == num)
+						{
+							Console.WriteLine($"Chunk {num} acknowledged.");
+							isContinue = true;
+						}
+					}
+				}
+			}
+
+			Console.WriteLine("Data sent to client.");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("Error: " + ex.Message);
+		}
+	}
+}
